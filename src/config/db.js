ae3fa90177c344e3ee6e7ddc7ad0a5d47e1c91f9
Maxime <maxime.lpz@outlook.fr>
2026-02-09 13:13:58 +0100

    connexion base de données

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,18 @@
 import mongoose from 'mongoose';
+import dns from 'dns';
+
+// Forcer Node à utiliser Google DNS
+dns.setServers(['8.8.8.8']);
+dns.setDefaultResultOrder('ipv4first');
 
 export const connectDB = async () => {
+  if (mongoose.connection.readyState === 1) return;
+
   try {
-    if (mongoose.connection.readyState === 1) return;
-
     await mongoose.connect(process.env.MONGO_URI);
-
     console.log('✅ MongoDB connecté');
   } catch (error) {
     console.error('❌ Erreur de connexion MongoDB :', error);
+    throw error; // Arrête le serveur si la DB n’est pas accessible
   }
 };

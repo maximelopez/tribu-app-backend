e6583e339bb1648df64aaa4a445fc77abf8e12e9
Maxime <maxime.lpz@outlook.fr>
2026-05-06 11:23:58 +0200

    fix routes place

--- src/routes/place.routes.js
+++ src/routes/place.routes.js
@@ -3,6 +3,6 @@ import { getPlacesByCity } from '../services/place.service.js';
 
 const router = Router();
 
-router.get('/', getPlacesByVille);
+router.get('/', getPlacesByCity);
 
 export default router;
